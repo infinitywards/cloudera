4fddad2d28a22cc5f8d41b730922ba305b195c4b
infinitywards <54397109+infinitywards@users.noreply.github.com>
2019-08-26 12:50:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,34 +28,24 @@ class App extends React.Component{
   render(){
     // console.log(this.state)
     return(
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Flame</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="index.css">
-</head>
-
-<body>
-    <div class="login-clean">
-        <form method="post">
-            <h2 class="sr-only">Flame</h2>
-            <div class="illustration"><i class="icon ion-ios-navigate"></i></div>
-            <div class="form-group"><input class="form-control" type="textarea" id="msg" placeholder="Enter Your Message" onChange= {this.handleMsgChange}></div>
-            <div class="form-group"><input type="file" id="uploadedFile" name="uploadedFile" onChange= {this.handleFileChange}></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="submit" value="SUBMIT" onClick={this.serve_uploadExcelAndMessage}>Send</button></div><a class="forgot" href="#">Copyright - Stark</a></form>
-				
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>)
-    
-    
+      <div>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+        <title>Flame</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
+        <link rel="stylesheet" href="index.css" />
+        <div className="login-clean">
+          <form method="post">
+            <h2 className="sr-only">Flame</h2>
+            <div className="illustration"><i className="icon ion-ios-navigate" /></div>
+            <div className="form-group"><input className="form-control" type="textarea" id="msg" placeholder="Enter Your Message" onchange="{this.handleMsgChange}" /></div>
+            <div className="form-group"><input type="file" id="uploadedFile" name="uploadedFile" onchange="{this.handleFileChange}" /></div>
+            <div className="form-group"><button className="btn btn-primary btn-block" type="submit" name="submit" value="SUBMIT" onclick="{this.serve_uploadExcelAndMessage}">Send</button></div><a className="forgot" href="#">Copyright - Stark</a></form>
+        </div>
+      </div>
+    ) 
   }
-
 }
 
 export default App;
