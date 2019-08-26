4fa9741e2397e9a0ab4b624621b069317f0a3aa9
infinitywards <54397109+infinitywards@users.noreply.github.com>
2019-08-26 13:04:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,14 +34,13 @@ class App extends React.Component{
         <title>Flame</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
-        <link rel="stylesheet" href="index.css" />
         <div className="login-clean">
           <form method="post">
             <h2 className="sr-only">Flame</h2>
             <div className="illustration"><i className="icon ion-ios-navigate" /></div>
-            <div className="form-group"><input className="form-control" type="textarea" id="msg" placeholder="Enter Your Message" onchange="{this.handleMsgChange}" /></div>
-            <div className="form-group"><input type="file" id="uploadedFile" name="uploadedFile" onchange="{this.handleFileChange}" /></div>
-            <div className="form-group"><button className="btn btn-primary btn-block" type="submit" name="submit" value="SUBMIT" onclick="{this.serve_uploadExcelAndMessage}">Send</button></div><a className="forgot" href="#">Copyright - Stark</a></form>
+            <div className="form-group"><input className="form-control" type="textarea" id="msg" placeholder="Enter Your Message" onchange={this.handleMsgChange} /></div>
+            <div className="form-group"><input type="file" id="uploadedFile" name="uploadedFile" onchange={this.handleFileChange} /></div>
+            <div className="form-group"><button className="btn btn-primary btn-block" type="submit" name="submit" value="SUBMIT" onclick={this.serve_uploadExcelAndMessage}>Send</button></div><a className="forgot" href="#">Copyright - Stark</a></form>
         </div>
       </div>
     ) 
